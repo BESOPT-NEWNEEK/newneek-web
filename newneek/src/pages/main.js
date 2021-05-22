7982c12f0ff1339aaedcfadbf5c4c669dcacd667
kimyeongseo <oseo0202@naver.com>
2021-05-22 15:14:53 +0900

    feat: grid card

--- newneek/src/pages/main.js
+++ newneek/src/pages/main.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const MainPage = () => {
-    return;
+const MainWrap = styled.div`
+display: flex;
+justify-content: center;
+section{
+    width: 1000px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+    justify-items: center;
+    @media (max-width: 1300px){ 
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media (max-width: 760px){
+        grid-template-columns: repeat(1, 1fr);
+    }
+}
+`;
+
+function MainPage() {
+    const [articleData, setArticleData] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const data = await getCardData();
+            setArticleData(data);
+        })();
+    }, []);
+
+    return (
+        <MainWrap>
+            <section>
+                {articleData &&
+                    articleData.map(article => <Card
+                        key={article.id}
+                        title={article.title}
+                        date={article.date}
+                        category={article.category}
+                        summary={article.summary} />)}
+            </section>
+        </MainWrap>
+    );
 }
 
 export default MainPage;
