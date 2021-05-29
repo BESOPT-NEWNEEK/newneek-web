7887f57be2d7ea705cc07ca24277e62d9dada50a
kimyeongseo <oseo0202@naver.com>
2021-05-29 18:14:15 +0900

    main page title component

---
+++ newneek/src/components/detail/Title.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const TitleWrap = styled.div`
+font-size: 1.8rem;
+font-weight: 700;
+text-align: center;
+margin: 5rem;
+`
+
+const Title = () => {
+    return (
+        <TitleWrap>
+            환경 · 에너지
+        </TitleWrap>
+    );
+};
+
+export default Title;
