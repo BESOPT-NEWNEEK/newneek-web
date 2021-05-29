f6abca9994417b2597ff00cb92df52936cb39b5d
kimyeongseo <oseo0202@naver.com>
2021-05-29 18:10:29 +0900

    reponsive component

---
+++ newneek/src/components/common/Responsive.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+const Responsive = ({ children }) => {
+    return (
+        <Container>
+            {children}
+        </Container>
+    );
+};
+
+export default Responsive;
+
+const Container = styled.div`
+  width:100%;
+  max-width: 1200px;
+  height: 215px;
+  margin: 0 auto;
+  @media screen and (max-width: 1200px){
+    max-width: 990px;
+  };
+  @media screen and (max-width:990px){
+    max-width: 768px;
+  }
+  @media screen and (max-width:768px){
+    max-width: 500px;
+  }
+  @media screen and (max-width:500px){
+    padding: 0 16px;
+  }
+`;
